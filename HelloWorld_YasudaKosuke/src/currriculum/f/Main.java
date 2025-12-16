de131f04dcfb34a90842a4eef8c396c00b72c8df
yasuda kousuke <ofuton1128@gmail.com>
2025-12-16 11:41:42 +0900

    Curriculum.f 課題 修正版

---
+++ HelloWorld_YasudaKosuke/src/currriculum/f/Main.java
@@ -0,0 +1,15 @@
+package currriculum.f;
+
+public class Main {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		
+		// 値を設定
+		emp.employeeId = "E001";
+		emp. name = "佐藤太郎";
+		
+		// 呼び出して出力
+		emp.showinfo();
+	}
+	
+}
