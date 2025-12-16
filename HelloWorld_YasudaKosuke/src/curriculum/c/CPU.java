45d0a74ced07d0ead6927417df6b206956f92d71
yasuda kousuke <ofuton1128@gmail.com>
2025-12-16 11:37:39 +0900

    Question6 じゃんけん課題 修正版

--- HelloWorld_YasudaKosuke/src/curriculum/c/CPU.java
+++ HelloWorld_YasudaKosuke/src/curriculum/c/CPU.java
@@ -10,4 +10,11 @@ public class CPU {
 	int getHand () {
 		return random.nextInt(3); // 0,1,2のみなので3を入れておく
 	}
+	/*
+	 * ソースが重複しており、Playerクラスにまとめたため削除
+	 * // それぞれの手の名前を返す
+		String handName(int hand) { // 数字を文字列に変換
+			if(hand == 0) return "グー";
+			if(hand == 1) return "チョキ";
+			return "パー";*/
 }
